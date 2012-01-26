cadea2234c17cbf6766b2b4ab7067461f85eec62
Peter Götz <peter.s.goetz@googlemail.com>
2012-01-26 08:44:12 +0100

    formatted code

--- src/graph/Plot.java
+++ src/graph/Plot.java
@@ -49,9 +49,8 @@ public final class Plot {
   private final int end_time;
 
   /** All the DataPoints we want to plot. */
-  private ArrayList<DataPoints> datapoints =
-    new ArrayList<DataPoints>();
-  
+  private ArrayList<DataPoints> datapoints = new ArrayList<DataPoints>();
+
   private List<Annotation> annotations = new ArrayList<Annotation>();
 
   /** Per-DataPoints Gnuplot options. */
@@ -74,7 +73,8 @@ public final class Plot {
    * Gnuplot always renders timestamps in UTC, so we simply apply a delta
    * to get local time.
    */
-  private final short utc_offset;
+  private static final int utc_offset = TimeZone.getDefault().getOffset(
+      System.currentTimeMillis()) / 1000;
 
   /**
    * Constructor.
@@ -104,7 +104,7 @@ public final class Plot {
       throw new IllegalArgumentException("Invalid end time: " + end_time);
     } else if (start_time >= end_time) {
       throw new IllegalArgumentException("start time (" + start_time
-        + ") is greater than or equal to end time: " + end_time);
+          + ") is greater than or equal to end time: " + end_time);
     }
     this.start_time = (int) start_time;
     this.end_time = (int) end_time;
@@ -143,7 +143,7 @@ public final class Plot {
     if (width < MIN_PIXELS || height < MIN_PIXELS) {
       final String what = width < MIN_PIXELS ? "width" : "height";
       throw new IllegalArgumentException(what + " smaller than " + MIN_PIXELS
-                                         + " in " + width + 'x' + height);
+          + " in " + width + 'x' + height);
     }
     this.width = width;
     this.height = height;
@@ -154,8 +154,7 @@ public final class Plot {
    * @param datapoints The data points to plot.
    * @param options The options to apply to this specific series.
    */
-  public void add(final DataPoints datapoints,
-                  final String options) {
+  public void add(final DataPoints datapoints, final String options) {
     // Technically, we could check the number of data points in the
     // datapoints argument in order to do something when there are none, but
     // this is potentially expensive with a SpanGroup since it requires
@@ -241,7 +240,7 @@ public final class Plot {
    * Can be {@code null} if there's no data to plot.
    */
   private void writeGnuplotScript(final String basepath,
-                                  final String[] datafiles) throws IOException {
+      final String[] datafiles) throws IOException {
     final String script_path = basepath + ".gnuplot";
     final PrintWriter gp = new PrintWriter(script_path);
     try {
@@ -276,17 +275,16 @@ public final class Plot {
                 + "set xtic rotate\n"
                 + "set output \"").append(basepath + ".png").append("\"\n"
                 + "set xrange [\"")
-        .append(String.valueOf((start_time & UNSIGNED) + utc_offset))
+        .append(String.valueOf(start_time + utc_offset))
         .append("\":\"")
-        .append(String.valueOf((end_time & UNSIGNED) + utc_offset))
+        .append(String.valueOf(end_time + utc_offset))
         .append("\"]\n");
       if (!params.containsKey("format x")) {
         gp.append("set format x \"").append(xFormat()).append("\"\n");
       }
       final int nseries = datapoints.size();
       if (nseries > 0) {
-        gp.write("set grid\n"
-                 + "set style data linespoints\n");
+        gp.write("set grid\n" + "set style data linespoints\n");
         if (!params.containsKey("key")) {
           gp.write("set key right box\n");
         }
@@ -302,8 +300,7 @@ public final class Plot {
           final String key = entry.getKey();
           final String value = entry.getValue();
           if (value != null) {
-            gp.append("set ").append(key)
-              .append(' ').append(value).write('\n');
+            gp.append("set ").append(key).append(' ').append(value).write('\n');
           } else {
             gp.append("unset ").append(key).write('\n');
           }
@@ -316,13 +313,17 @@ public final class Plot {
           break;
         }
       }
-      
-      for(Annotation annotation : annotations) {
+
+      for (Annotation annotation : annotations) {
         String ts = Long.toString(annotation.getTimestamp());
         String value = new String(annotation.getValue());
-        gp.append("set arrow from \"").append(ts).append("\", graph 0 to \"").append(ts).append("\", graph 1 nohead ls 3\n");
-        gp.append("set object rectangle at \"").append(ts).append("\", graph 0 size char (strlen(\"").append(value).append("\") + 3), char 1 front fc rgbcolor \"white\"\n");
-        gp.append("set label \"").append(value).append("\" at \"").append(ts).append("\", graph 0 front center\n");
+        gp.append("set arrow from \"").append(ts).append("\", graph 0 to \"")
+            .append(ts).append("\", graph 1 nohead ls 3\n");
+        gp.append("set object rectangle at \"").append(ts)
+            .append("\", graph 0 size char (strlen(\"").append(value)
+            .append("\") + 3), char 1 front fc rgbcolor \"white\"\n");
+        gp.append("set label \"").append(value).append("\" at \"").append(ts)
+            .append("\", graph 0 front center\n");
       }
 
       gp.write("plot ");
