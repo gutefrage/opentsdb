64f99e5269ea10ab813dd3d99f7208c04712a6dd
Peter Götz <peter.s.goetz@googlemail.com>
2012-04-10 14:33:23 +0200

    removed NPE in GWT code when working with the JSON result for a query (possibly a consequence of the annotations being stored inside the 'tsdb' table)

--- src/tsd/client/QueryUi.java
+++ src/tsd/client/QueryUi.java
@@ -1019,8 +1019,10 @@ public class QueryUi implements EntryPoint, HistoryListener {
             graph.setUrl(uri + "&png");
             graph.setVisible(true);
 
-            msg += result.get("points").isNumber() + " points retrieved, "
-                + nplotted + " points plotted";
+            if (result.get("points") != null && result.get("points").isNumber() != null) {
+              msg += result.get("points").isNumber() + " points retrieved, "
+                  + nplotted + " points plotted";
+            }
           } else {
             graph.setVisible(false);
             msg += "Your query didn't return anything";
